1e09e523f433e4873216dc4a8f7840e3e665372e
m1gw1 <migwi.eth>
2022-07-08 00:25:46 -0400

    add mul test

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,6 +1,11 @@
-from afang.main import calc_addition
+from afang.main import calc_addition, calc_mul
 
 
 def test_calc_addition():
     output = calc_addition(2, 4)
     assert output == 6
+
+
+def test_calc_mul():
+    output = calc_mul(2, 4)
+    assert output == 8
