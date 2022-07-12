9b545d742dd81099d2c99e80d21bd7f8b78d921a
migwi.eth <108780582+m1gw1@users.noreply.github.com>
2022-07-12 03:01:45 -0400

    Add ohlcv db (#10)
    * add logging config
    
    * add ohlcv database
    
    Co-authored-by: m1gw1 <migwi.eth>

---
+++ afang/database/ohlcv_database.py
@@ -0,0 +1,241 @@
+import logging
+import os
+import pathlib
+import time
+from typing import Any, List, Tuple, Union
+
+import h5py
+import numpy as np
+import pandas as pd
+
+logger = logging.getLogger()
+
+
+class OHLCVDatabase:
+    """Interface to store, retrieve, and manipulate price data in a HDF5
+    database."""
+
+    def __init__(self, root_db_dir: str, exchange: str, symbol: str) -> None:
+        """Initialize the OHLCVDatabase class.
+
+        :param root_db_dir: root database directory.
+        :param exchange: name of exchange to use.
+        :param symbol: symbol to use.
+        """
+
+        if not root_db_dir:
+            root_db_dir = f"{pathlib.Path(__file__).parents[2]}/data/ohlcv"
+
+        # Create exchange database directory if it does not exist.
+        exchange_db_dir = f"{root_db_dir}/{exchange}"
+        if not os.path.exists(exchange_db_dir):
+            os.mkdir(exchange_db_dir)
+
+        self.hf = h5py.File(f"{exchange_db_dir}/{symbol}.h5", "a")
+        self.hf.flush()
+
+    def create_dataset(self, symbol: str) -> None:
+        """Create a dataset in the exchange's HDF5 database to contain symbol
+        price data information if one does not already exist.
+
+        :param symbol: symbol to create a dataset for.
+
+        :return: None
+        """
+
+        if symbol in self.hf.keys():
+            return
+
+        self.hf.create_dataset(
+            name=symbol, shape=(0, 6), maxshape=(None, 6), dtype="float64"
+        )
+        self.hf.flush()
+
+    def get_min_max_timestamp(
+        self, symbol: str
+    ) -> Union[Tuple[None, None], Tuple[float, float]]:
+        """Fetch the minimum(oldest) and maximum(latest) timestamp in a
+        symbol's stored price data. If no dataset exists for the provided
+        symbol, (None, None) will be returned.
+
+        :param symbol: name of symbol to use.
+
+        :return: Union[Tuple[None, None], Tuple[float, float]]
+        """
+
+        if symbol not in self.hf.keys():
+            return None, None
+
+        existing_data = self.hf.get(symbol)[:]
+        if not len(existing_data):
+            return None, None
+
+        min_timestamp = min(existing_data, key=lambda row: row[0])[0]
+        max_timestamp = max(existing_data, key=lambda row: row[0])[0]
+
+        return min_timestamp, max_timestamp
+
+    def write_data(
+        self, symbol: str, data: List[Tuple[float, float, float, float, float, float]]
+    ) -> None:
+        """Write OHLCV price data into a symbol's dataset within an exchange's
+        HDF5 database.
+
+        :param symbol: symbol whose price data is to be stored.
+        :param data: list of OHLCV tuples to be stored into the symbol's dataset.
+
+        :return: None
+        """
+
+        if symbol not in self.hf.keys():
+            logger.warning("%s: no dataset exists for symbol in database", symbol)
+            return
+
+        if not len(data):
+            return
+
+        min_timestamp, max_timestamp = self.get_min_max_timestamp(symbol)
+        if min_timestamp is None:
+            min_timestamp = float("inf")
+            max_timestamp = 0
+
+        filtered_data = []
+        for d in data:
+            if d[0] < min_timestamp:
+                filtered_data.append(d)
+            elif d[0] > max_timestamp:
+                filtered_data.append(d)
+
+        if not len(filtered_data):
+            logger.warning("%s: no data to insert into database", symbol)
+            return
+
+        if len(filtered_data) != len(data):
+            logger.warning(
+                "%s: length of filtered data(%s) does not match length of input data(%s)",
+                symbol,
+                len(filtered_data),
+                len(data),
+            )
+            return
+
+        data_array = np.array(filtered_data)
+
+        self.hf.get(symbol).resize(
+            self.hf.get(symbol).shape[0] + data_array.shape[0], axis=0
+        )
+        idx = -data_array.shape[0]
+        self.hf.get(symbol)[idx:] = data_array
+        self.hf.flush()
+
+    def get_data(
+        self, symbol: str, from_time: int, to_time: int
+    ) -> Union[pd.DataFrame, None]:
+        """Retrieve price data from the database for a given symbol sorted by
+        timestamps and within the from_time and to_time parameters.
+
+        :param symbol: name of symbol whose data is to be retrieved.
+        :param from_time: UNIX timestamp in ms which retrieved price data should not be older than.
+        :param to_time: UNIX timestamp in ms which retrieved price data should not be newer than.
+
+        :return: Union[None, pd.DataFrame]
+        """
+
+        if symbol not in self.hf.keys():
+            logger.warning("%s: no dataset exists for symbol in database", symbol)
+            return None
+
+        query_start_time = time.time()
+
+        existing_data = self.hf.get(symbol)[:]
+        if not len(existing_data):
+            return None
+
+        data = sorted(existing_data, key=lambda row: row[0])
+        data = np.array(data)
+
+        df = pd.DataFrame(
+            data, columns=["timestamp", "open", "high", "low", "close", "volume"]
+        )
+        df = df[(df.timestamp >= from_time) & (df.timestamp <= to_time)]
+
+        df.timestamp = pd.to_datetime(df.timestamp.values.astype(np.int64), unit="ms")
+        df.set_index("timestamp", drop=True, inplace=True)
+
+        query_time = round(time.time() - query_start_time, 2)
+
+        logger.info(
+            "Retrieved %s %s data candles from the database in %s seconds",
+            len(df.index),
+            symbol,
+            query_time,
+        )
+
+        return df
+
+    @classmethod
+    def _is_unique(
+        cls, df_col: pd.Series, allowed_vals: Any, astype: str = "timedelta64[m]"
+    ) -> List[str]:
+        """Check if all values in a pandas' series are of a specific allowed
+        set. This function will return a list of disallowed items.
+
+        :param df_col: dataframe column.
+        :param allowed_vals: dict of values allowed in the pandas' series.
+        :param astype: type to cast disallowed values to.
+
+        :return: List[str]
+        """
+
+        value_counts = df_col.value_counts()
+        disallowed_values = set(df_col.unique()) - allowed_vals
+
+        verbose_disallowed_values = list()
+        for disallowed_value in disallowed_values:
+            verbose_disallowed_values.append(
+                f"{disallowed_value.astype(astype)} ({value_counts[disallowed_value]})"
+            )
+
+        return verbose_disallowed_values
+
+    def is_dataset_valid(self, symbol: str) -> bool:
+        """Ensure that persisted price data of a given symbol is valid i.e.
+        timestamps are strictly 1 or 2 minute(s) apart.
+
+        :param symbol: name of symbol whose data is to be validated.
+
+        :return: bool
+        """
+
+        if symbol not in self.hf.keys():
+            logger.warning("%s: no dataset exists for symbol in database", symbol)
+            return False
+
+        existing_data = self.hf.get(symbol)[:]
+        if not len(existing_data):
+            return False
+
+        data = sorted(existing_data, key=lambda row: row[0])
+        data = np.array(data)
+
+        df = pd.DataFrame(
+            data, columns=["timestamp", "open", "high", "low", "close", "volume"]
+        )
+        df.timestamp = pd.to_datetime(df.timestamp.values.astype(np.int64), unit="ms")
+
+        df["open-time-diff"] = df["timestamp"].shift(-1) - df["timestamp"]
+        allowed_values = {
+            np.timedelta64(60000000000, "ns"),  # 1 minute
+            np.timedelta64(120000000000, "ns"),  # 2 minutes
+        }
+        disallowed_values = self._is_unique(df["open-time-diff"][:-1], allowed_values)
+        if disallowed_values:
+            logger.warning(
+                "%s: invalid symbol dataset. disallowed values: %s",
+                symbol,
+                disallowed_values,
+            )
+            return False
+
+        logger.info("%s: symbol dataset is valid", symbol)
+        return True
---
+++ tests/database/ohlcv_database_test.py
@@ -0,0 +1,232 @@
+import os
+from typing import List, Tuple
+
+import pandas as pd
+import pytest
+from pandas.testing import assert_frame_equal
+
+from afang.database.ohlcv_database import OHLCVDatabase
+
+
+@pytest.fixture
+def ohlcv_write_data() -> List[Tuple[float, float, float, float, float, float]]:
+    return [
+        (
+            1657510380000,
+            10.2,
+            12.5,
+            9.5,
+            10.2,
+            100.5,
+        ),  # Monday, July 11, 2022 3:33:00 AM
+        (
+            1657510320000,
+            10.5,
+            12.5,
+            9.5,
+            10.2,
+            100.5,
+        ),  # Monday, July 11, 2022 3:32:00 AM
+    ]
+
+
+def test_symbol_database_creation(ohlcv_root_db_dir) -> None:
+    # test that database is created if it doesn't yet exist.
+    OHLCVDatabase(ohlcv_root_db_dir, "test_exchange", "test_symbol")
+    assert os.path.exists(f"{ohlcv_root_db_dir}/test_exchange") is True
+    assert os.path.exists(f"{ohlcv_root_db_dir}/test_exchange/test_symbol.h5") is True
+
+    # test that a new symbol database can be created under the same exchange.
+    OHLCVDatabase(ohlcv_root_db_dir, "test_exchange", "another_test_symbol")
+    assert os.path.exists(f"{ohlcv_root_db_dir}/test_exchange") is True
+    assert (
+        os.path.exists(f"{ohlcv_root_db_dir}/test_exchange/another_test_symbol.h5")
+        is True
+    )
+
+
+def test_symbol_dataset_creation(ohlcv_root_db_dir) -> None:
+    ohlcv_db = OHLCVDatabase(ohlcv_root_db_dir, "test_exchange", "test_symbol")
+    assert "test_symbol" not in ohlcv_db.hf.keys()
+
+    ohlcv_db.create_dataset("test_symbol")
+    assert "test_symbol" in ohlcv_db.hf.keys()
+
+
+def test_write_to_non_existent_dataset(ohlcv_root_db_dir, caplog) -> None:
+    ohlcv_db = OHLCVDatabase(ohlcv_root_db_dir, "test_exchange", "test_symbol")
+    ohlcv_db.write_data("non_existent_symbol", [])
+
+    assert caplog.records[0].levelname == "WARNING"
+    assert (
+        "non_existent_symbol: no dataset exists for symbol in database" in caplog.text
+    )
+
+
+@pytest.mark.parametrize(
+    "min_ts, max_ts, expect_log, expected_log_text, expected_data_len",
+    [
+        (1657510320000, 1657510380000, True, "no data to insert into database", 2),
+        (1657510320000, 1657510320000, True, "does not match length of input data", 2),
+        (1657510300000, 1657510300000, False, "", 4),
+    ],
+)
+def test_write_to_dataset(
+    mocker,
+    ohlcv_root_db_dir,
+    caplog,
+    ohlcv_write_data,
+    min_ts,
+    max_ts,
+    expect_log,
+    expected_log_text,
+    expected_data_len,
+) -> None:
+    # set up dataset for a symbol and write initial data into the dataset.
+    ohlcv_db = OHLCVDatabase(ohlcv_root_db_dir, "test_exchange", "test_symbol")
+    ohlcv_db.create_dataset("test_symbol")
+    ohlcv_db.write_data("test_symbol", ohlcv_write_data)
+
+    # mock the return value of the get_min_max_timestamp function.
+    def mock_get_min_max_timestamp(_self, _symbol):
+        return min_ts, max_ts
+
+    mocker.patch(
+        "afang.database.ohlcv_database.OHLCVDatabase.get_min_max_timestamp",
+        mock_get_min_max_timestamp,
+    )
+
+    # attempt to write the test data into the dataset.
+    ohlcv_db.write_data("test_symbol", ohlcv_write_data)
+    dataset_data_len = len(ohlcv_db.hf.get("test_symbol")[:])
+
+    assert dataset_data_len == expected_data_len
+    if expect_log:
+        assert expected_log_text in caplog.text
+
+
+def test_get_min_max_timestamp_no_dataset(ohlcv_root_db_dir) -> None:
+    ohlcv_db = OHLCVDatabase(ohlcv_root_db_dir, "test_exchange", "test_symbol")
+    assert ohlcv_db.get_min_max_timestamp("test_symbol") == (None, None)
+
+
+def test_get_min_max_timestamp_empty_dataset(ohlcv_root_db_dir) -> None:
+    ohlcv_db = OHLCVDatabase(ohlcv_root_db_dir, "test_exchange", "test_symbol")
+    ohlcv_db.create_dataset("test_symbol")
+    assert ohlcv_db.get_min_max_timestamp("test_symbol") == (None, None)
+
+
+@pytest.mark.parametrize(
+    "data, min_ts, max_ts",
+    [
+        ([(1657510320000, 10.5, 12.5, 9.5, 10.2, 100.5)], 1657510320000, 1657510320000),
+        (
+            [
+                (1657510320000, 10.5, 12.5, 9.5, 10.2, 100.5),
+                (1657510380000, 10.2, 12.5, 9.5, 10.2, 100.5),
+            ],
+            1657510320000,
+            1657510380000,
+        ),
+    ],
+)
+def test_get_min_max_timestamp(ohlcv_root_db_dir, data, min_ts, max_ts) -> None:
+    ohlcv_db = OHLCVDatabase(ohlcv_root_db_dir, "test_exchange", "test_symbol")
+    ohlcv_db.create_dataset("test_symbol")
+    ohlcv_db.write_data("test_symbol", data)
+
+    assert ohlcv_db.get_min_max_timestamp("test_symbol") == (min_ts, max_ts)
+
+
+def test_get_data_no_dataset(ohlcv_root_db_dir) -> None:
+    ohlcv_db = OHLCVDatabase(ohlcv_root_db_dir, "test_exchange", "test_symbol")
+    data = ohlcv_db.get_data("test_symbol", 0, 100)
+    assert data is None
+
+
+def test_get_data_empty_dataset(ohlcv_root_db_dir) -> None:
+    ohlcv_db = OHLCVDatabase(ohlcv_root_db_dir, "test_exchange", "test_symbol")
+    ohlcv_db.create_dataset("test_symbol")
+    data = ohlcv_db.get_data("test_symbol", 0, 100)
+    assert data is None
+
+
+def test_get_data(ohlcv_root_db_dir, caplog) -> None:
+    ohlcv_data: List[Tuple[float, float, float, float, float, float]] = [
+        (5, 40.2, 92.5, 8.5, 12.2, 190.5),
+        (1, 10.2, 12.5, 9.5, 14.2, 120.5),
+        (4, 70.2, 42.5, 1.5, 15.2, 160.5),
+        (2, 30.2, 72.5, 4.5, 16.2, 130.5),
+        (3, 60.2, 62.5, 2.5, 18.2, 150.5),
+    ]
+
+    ohlcv_db = OHLCVDatabase(ohlcv_root_db_dir, "test_exchange", "test_symbol")
+    ohlcv_db.create_dataset("test_symbol")
+    ohlcv_db.write_data("test_symbol", ohlcv_data)
+    data = ohlcv_db.get_data("test_symbol", 2, 4)
+
+    expected_data = {
+        "open": [30.2, 60.2, 70.2],
+        "high": [72.5, 62.5, 42.5],
+        "low": [4.5, 2.5, 1.5],
+        "close": [16.2, 18.2, 15.2],
+        "volume": [130.5, 150.5, 160.5],
+    }
+    ts_data = [
+        "1970-01-01 00:00:00.002",
+        "1970-01-01 00:00:00.003",
+        "1970-01-01 00:00:00.004",
+    ]
+    expected_data_df = pd.DataFrame(data=expected_data)
+    expected_data_df.set_index(pd.DatetimeIndex(ts_data), drop=True, inplace=True)
+    expected_data_df.index.name = "timestamp"
+
+    assert_frame_equal(data, expected_data_df, check_dtype=True)
+
+
+def test_is_data_valid_no_dataset(ohlcv_root_db_dir, caplog) -> None:
+    ohlcv_db = OHLCVDatabase(ohlcv_root_db_dir, "test_exchange", "test_symbol")
+    is_valid = ohlcv_db.is_dataset_valid("test_symbol")
+
+    assert "no dataset exists for symbol in database" in caplog.text
+    assert not is_valid
+
+
+def test_is_data_valid_empty_dataset(ohlcv_root_db_dir) -> None:
+    ohlcv_db = OHLCVDatabase(ohlcv_root_db_dir, "test_exchange", "test_symbol")
+    ohlcv_db.create_dataset("test_symbol")
+    is_valid = ohlcv_db.is_dataset_valid("test_symbol")
+
+    assert not is_valid
+
+
+@pytest.mark.parametrize(
+    "data, expect_is_valid",
+    [
+        (
+            [
+                (1657510560000, 10.5, 12.5, 9.5, 10.2, 100.5),
+                (1657510380000, 10.2, 12.5, 9.5, 10.2, 100.5),
+                (1657510320000, 10.5, 12.5, 9.5, 10.2, 100.5),
+                (1657510500000, 10.5, 12.5, 9.5, 10.2, 100.5),
+            ],
+            True,
+        ),
+        (
+            [
+                (1657510560000, 10.5, 12.5, 9.5, 10.2, 100.5),
+                (1657510500000, 10.2, 12.5, 9.5, 10.2, 100.5),
+                (1657510320000, 10.5, 12.5, 9.5, 10.2, 100.5),
+                (1657510500000, 10.5, 12.5, 9.5, 10.2, 100.5),
+            ],
+            False,
+        ),
+    ],
+)
+def test_is_data_valid(ohlcv_root_db_dir, caplog, data, expect_is_valid) -> None:
+    ohlcv_db = OHLCVDatabase(ohlcv_root_db_dir, "test_exchange", "test_symbol")
+    ohlcv_db.create_dataset("test_symbol")
+    ohlcv_db.write_data("test_symbol", data)
+    is_valid = ohlcv_db.is_dataset_valid("test_symbol")
+
+    assert is_valid == expect_is_valid
