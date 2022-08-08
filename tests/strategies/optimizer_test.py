54a16160371a104ecac53010a67b87905c13f816
m1gw1 <migwi.eth>
2022-08-08 08:55:19 -0400

    add test evaluate population

--- tests/strategies/optimizer_test.py
+++ tests/strategies/optimizer_test.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 def test_generate_initial_population(dummy_is_optimizer) -> None:
     initial_population = dummy_is_optimizer.generate_initial_population()
 
@@ -11,3 +14,73 @@ def test_generate_initial_population(dummy_is_optimizer) -> None:
         assert 0.05 <= backtest_params["psar_max_val"] <= 0.3
         assert 0.01 <= backtest_params["psar_acceleration"] <= 0.08
         assert backtest_params["psar_max_val"] >= backtest_params["psar_acceleration"]
+
+
+@pytest.mark.parametrize(
+    "analysis_result, modified_result",
+    [
+        (
+            [
+                {
+                    "total_trades": {"all_trades": 0},
+                    "average_pnl": {"all_trades": 0, "positive_optimization": True},
+                    "maximum_drawdown": {
+                        "all_trades": 0,
+                        "positive_optimization": False,
+                    },
+                }
+            ],
+            [
+                {
+                    "average_pnl": {
+                        "all_trades": float("-inf"),
+                        "positive_optimization": True,
+                    },
+                    "maximum_drawdown": {
+                        "all_trades": float("inf"),
+                        "positive_optimization": False,
+                    },
+                    "total_trades": {"all_trades": 0},
+                }
+            ],
+        ),
+        (
+            [
+                {
+                    "total_trades": {"all_trades": 1},
+                    "average_pnl": {"all_trades": 100, "positive_optimization": True},
+                    "maximum_drawdown": {
+                        "all_trades": 12,
+                        "positive_optimization": False,
+                    },
+                }
+            ],
+            [
+                {
+                    "average_pnl": {
+                        "all_trades": 100,
+                        "positive_optimization": True,
+                    },
+                    "maximum_drawdown": {
+                        "all_trades": 12,
+                        "positive_optimization": False,
+                    },
+                    "total_trades": {"all_trades": 1},
+                }
+            ],
+        ),
+    ],
+)
+def test_evaluate_population(
+    mocker, dummy_is_optimizer, analysis_result, modified_result
+) -> None:
+    mocked_run_backtest = mocker.patch(
+        "afang.strategies.backtester.Backtester.run_backtest",
+        return_value=analysis_result,
+    )
+    initial_population = dummy_is_optimizer.generate_initial_population()
+    evaluated_population = dummy_is_optimizer.evaluate_population(initial_population)
+
+    assert mocked_run_backtest.assert_called
+    for profile in evaluated_population:
+        assert profile.backtest_analysis == modified_result
