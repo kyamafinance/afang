79134775e0579689fb15caac658e76ac8d0127e1
m1gw1 <migwi.eth>
2022-08-09 01:42:08 -0400

    add test for generate offspring population

--- tests/strategies/optimizer_test.py
+++ tests/strategies/optimizer_test.py
@@ -1,4 +1,54 @@
+from typing import Dict, List
+
 import pytest
+
+from afang.strategies.optimizer import BacktestProfile
+
+
+@pytest.fixture
+def run_backtest_side_effect() -> List[List[Dict]]:
+    return [
+        [
+            {
+                "total_trades": {"all_trades": 1},
+                "average_pnl": {"all_trades": 100, "positive_optimization": True},
+                "maximum_drawdown": {
+                    "all_trades": 12,
+                    "positive_optimization": False,
+                },
+            }
+        ],
+        [
+            {
+                "total_trades": {"all_trades": 1},
+                "average_pnl": {"all_trades": 90, "positive_optimization": True},
+                "maximum_drawdown": {
+                    "all_trades": 16,
+                    "positive_optimization": False,
+                },
+            }
+        ],
+        [
+            {
+                "total_trades": {"all_trades": 1},
+                "average_pnl": {"all_trades": 80, "positive_optimization": True},
+                "maximum_drawdown": {
+                    "all_trades": 20,
+                    "positive_optimization": False,
+                },
+            }
+        ],
+        [
+            {
+                "total_trades": {"all_trades": 1},
+                "average_pnl": {"all_trades": 30, "positive_optimization": True},
+                "maximum_drawdown": {
+                    "all_trades": 28,
+                    "positive_optimization": False,
+                },
+            }
+        ],
+    ]
 
 
 def test_generate_initial_population(dummy_is_optimizer) -> None:
@@ -91,51 +141,12 @@ def test_calculate_crowding_distance_empty_population(dummy_is_optimizer) -> Non
     assert not result_population
 
 
-def test_calculate_crowding_distance(mocker, dummy_is_optimizer) -> None:
+def test_calculate_crowding_distance(
+    mocker, dummy_is_optimizer, run_backtest_side_effect
+) -> None:
     mocker.patch(
         "afang.strategies.backtester.Backtester.run_backtest",
-        side_effect=[
-            [
-                {
-                    "total_trades": {"all_trades": 1},
-                    "average_pnl": {"all_trades": 100, "positive_optimization": True},
-                    "maximum_drawdown": {
-                        "all_trades": 12,
-                        "positive_optimization": False,
-                    },
-                }
-            ],
-            [
-                {
-                    "total_trades": {"all_trades": 1},
-                    "average_pnl": {"all_trades": 90, "positive_optimization": True},
-                    "maximum_drawdown": {
-                        "all_trades": 16,
-                        "positive_optimization": False,
-                    },
-                }
-            ],
-            [
-                {
-                    "total_trades": {"all_trades": 1},
-                    "average_pnl": {"all_trades": 80, "positive_optimization": True},
-                    "maximum_drawdown": {
-                        "all_trades": 20,
-                        "positive_optimization": False,
-                    },
-                }
-            ],
-            [
-                {
-                    "total_trades": {"all_trades": 1},
-                    "average_pnl": {"all_trades": 30, "positive_optimization": True},
-                    "maximum_drawdown": {
-                        "all_trades": 28,
-                        "positive_optimization": False,
-                    },
-                }
-            ],
-        ],
+        side_effect=run_backtest_side_effect,
     )
 
     initial_population = dummy_is_optimizer.generate_initial_population()
@@ -152,3 +163,45 @@ def test_calculate_crowding_distance(mocker, dummy_is_optimizer) -> None:
     ]
     for i in range(len(result_population)):
         assert result_population[i].crowding_distance == expected_crowding_distances[i]
+
+
+@pytest.mark.parametrize(
+    "population",
+    [
+        [],
+        [BacktestProfile()],
+    ],
+)
+def test_generate_offspring_population_too_small_population(
+    dummy_is_optimizer, population
+) -> None:
+    expected_offspring = dummy_is_optimizer.generate_offspring_population(population)
+    assert expected_offspring == population
+
+
+def test_generate_offspring_population(
+    mocker, dummy_is_optimizer, run_backtest_side_effect
+) -> None:
+    mocker.patch(
+        "afang.strategies.backtester.Backtester.run_backtest",
+        side_effect=run_backtest_side_effect,
+    )
+
+    initial_population = dummy_is_optimizer.generate_initial_population()
+    evaluated_population = dummy_is_optimizer.evaluate_population(initial_population)
+    evaluated_population = dummy_is_optimizer.calculate_crowding_distance(
+        evaluated_population
+    )
+    offspring_population = dummy_is_optimizer.generate_offspring_population(
+        evaluated_population
+    )
+
+    assert len(offspring_population) == 4
+    assert len(dummy_is_optimizer.population_backtest_params) == 8
+    for profile in offspring_population:
+        backtest_params = profile.backtest_parameters
+        assert 1.0 <= backtest_params["RR"] <= 5.0
+        assert 100 <= backtest_params["ema_period"] <= 800
+        assert 0.05 <= backtest_params["psar_max_val"] <= 0.3
+        assert 0.01 <= backtest_params["psar_acceleration"] <= 0.08
+        assert backtest_params["psar_max_val"] >= backtest_params["psar_acceleration"]
