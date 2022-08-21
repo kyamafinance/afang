80673ea3e084b2a8aed26522b79c85b1e41eb3ae
migwi.eth <108780582+m1gw1@users.noreply.github.com>
2022-08-21 11:44:31 -0400

    Refactor rotating file handler logger (#67)
    refactor rotating file handler logger
    
    Co-authored-by: m1gw1 <migwi.eth>

--- afang/utils/logger.py
+++ afang/utils/logger.py
@@ -1,11 +1,13 @@
 import logging.config
 import os
 import pathlib
+from logging.handlers import RotatingFileHandler
 
 import yaml
 from dotenv import load_dotenv
 
 load_dotenv()
+logger = logging.getLogger()
 
 
 class Logger:
@@ -17,6 +19,30 @@ class Logger:
         self.default_config = os.path.join(
             pathlib.Path(__file__).parents[2], "config/logging_config.yaml"
         )
+
+    @classmethod
+    def get_rotating_file_handler(cls) -> RotatingFileHandler:
+        """Get a rotating file handler to use in production environments.
+
+        :return: RotatingFileHandler
+        """
+
+        rotating_file_handler = RotatingFileHandler(
+            "logs/logs.log",
+            maxBytes=10485760,
+            backupCount=20,
+            encoding="utf8",
+        )
+        rotating_file_handler.name = "file"
+        rotating_file_handler.setFormatter(
+            logging.Formatter(
+                "%(asctime)s %(levelname)s :: %(message)s",
+                datefmt="%m/%d/%Y %H:%M:%S",
+            )
+        )
+        rotating_file_handler.setLevel(logging.INFO)
+
+        return rotating_file_handler
 
     def setup_logger(self, default_level: int = logging.INFO) -> None:
         """Set up the application logger from the configuration present in
@@ -32,13 +58,12 @@ class Logger:
         if os.path.exists(path):
             with open(path) as file:
                 config = yaml.safe_load(file.read())
-
-                # disable the file handler if not in production.
-                if os.environ.get("ENV", "development") != "production":
-                    del config["handlers"]["file"]
-                    config["root"]["handlers"] = ["console"]
-
                 logging.config.dictConfig(config)
                 logging.captureWarnings(True)
+
+                # add a rotating file handler if in production environment.
+                if os.environ.get("ENV", "development") == "production":
+                    rotating_file_handler = self.get_rotating_file_handler()
+                    logger.addHandler(rotating_file_handler)
         else:
             logging.basicConfig(level=default_level)
