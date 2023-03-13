10d7a4e90c706e088eba2682f97885f6f032c3d7
m1gw1 <migwi.eth>
2023-03-13 05:44:15 -0400

    add generate_and_verify_trader_trade_levels test

--- tests/strategies/trader_test.py
+++ tests/strategies/trader_test.py
@@ -17,6 +17,7 @@ from afang.exchanges.models import Order as ExchangeOrder
 from afang.exchanges.models import OrderSide, OrderType, Symbol, SymbolBalance
 from afang.models import Timeframe
 from afang.strategies.is_strategy import IsStrategy
+from afang.strategies.models import TradeLevels
 from afang.strategies.trader import Trader
 
 
@@ -1203,6 +1204,38 @@ def test_handle_open_trade_positions(
     )
 
     assert mocked_place_close_trade_position_order.called
+
+
+@pytest.mark.parametrize(
+    "direction, candle_data, is_valid",
+    [
+        (1, {"close": 16, "open_time": 1}, False),
+        (1, {"close": 10, "open_time": 1}, True),
+        (-1, {"close": 16, "open_time": 1}, False),
+        (-1, {"close": 10, "open_time": 1}, True),
+    ],
+)
+def test_generate_and_verify_trader_trade_levels(
+    caplog, dummy_is_strategy, direction, candle_data, is_valid
+) -> None:
+    dummy_trade_levels = TradeLevels(entry_price=10, stop_price=5, target_price=20)
+
+    def dummy_generate_trade_levels(_data, _trade_signal_direction) -> TradeLevels:
+        return dummy_trade_levels
+
+    dummy_is_strategy.generate_trade_levels = dummy_generate_trade_levels
+
+    trade_levels = dummy_is_strategy.generate_and_verify_trader_trade_levels(
+        SimpleNamespace(**candle_data), 1
+    )
+
+    if not is_valid:
+        assert not trade_levels
+        assert caplog.records[-1].levelname == "WARNING"
+        assert "Generated trade levels are invalid" in caplog.text
+        return
+
+    assert trade_levels == dummy_trade_levels
 
 
 def test_handle_finalized_trade_positions(
